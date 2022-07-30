f2c9654a0a96aeeb5e39ab82df99f6e4f3cce415
HursitTarcan <hursittarcan@gmail.com>
2022-07-30 19:08:50 +0200

    template for challenge one added.

---
+++ challenge-one/template/app.js
@@ -0,0 +1,10 @@
+//CHALLENGE ONE - TEMPLATE
+
+function checkBills(bills) {
+    //TODO: Calculate the total of all elements except the last one.
+    //TODO: The last element is the total provided by Max himself.
+    //TODO: Check if both numbers are equal or not, and return a helpful message.
+}
+
+let bills = [30, 24, 65, 12, 124, 78, 85, 65, 650];
+console.log(checkBills(bills));
