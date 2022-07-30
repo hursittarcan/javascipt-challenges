0c3dccffd49ada4ca4a0007062a79de5a91fed5f
HursitTarcan <hursittarcan@gmail.com>
2022-07-30 19:17:31 +0200

    challenge two done.

---
+++ challenge-two/app.js
@@ -0,0 +1,8 @@
+function checkIfNumbersAreEqual(numberOne, numberTwo) {
+    return numberOne === numberTwo;
+}
+
+let numberOne = 15;
+let numberTwo = 15;
+
+console.log(checkIfNumbersAreEqual(numberOne, numberTwo));
