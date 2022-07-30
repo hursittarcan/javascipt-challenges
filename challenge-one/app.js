ae35da7cf74878671a8f29f2bc84f3c6c17ed7cc
HursitTarcan <hursittarcan@gmail.com>
2022-07-30 17:47:58 +0200

    first challenge complete.

--- challenge-one/app.js
+++ challenge-one/app.js
@@ -1,14 +1,25 @@
-const readline = require("readline");
+//write a function that accepts an (any length) array as a parameter; the function should sum all elements in the array
+//except the last index. The function than checks the last index and checks if the sum of all numbers equals this number.
+//If it equals, we return a message saying it is ok. If not we return a message with the difference between the two numbers.
 
-let name = prompt("Your name: ");
-let age = prompt("Your age: ");
-let gender = prompt("Gender: ");
-let carBrand = prompt("Brand of car: ");
-let enginePower = prompt("Engine power (in hp): ");
-let licenseYear = prompt("What year did you get your drivers license: ");
+function checkArray(array) {
+    let sum = 0;
+    let billsTotal = array.slice(-1).pop();
+    for (let i = 0; i < array.length - 1; i++) {
+        sum += array[i];
+    }
 
+    if (sum === billsTotal) {
+        return "Everything is ok!"
+    }
+    else if (sum < billsTotal) {
+        return "You are paying €" + (billsTotal - sum) + " too much.";
+    }
+    else {
+        return "Your calculated total is too less: " + (billsTotal - sum);
+    }
+}
 
+let array = [3, 10, 70];
 
-
-let array = [1, 2, 3, 6]
-
+console.log(checkArray(array));
