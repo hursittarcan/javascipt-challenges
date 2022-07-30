fc182d050c79bded7ec6d3a8c052f9ba3451c60a
HursitTarcan <hursittarcan@gmail.com>
2022-07-30 15:30:55 +0200

    app.js -> challenge one, updated

---
+++ challenge-one/app.js
@@ -0,0 +1,14 @@
+const readline = require("readline");
+
+let name = prompt("Your name: ");
+let age = prompt("Your age: ");
+let gender = prompt("Gender: ");
+let carBrand = prompt("Brand of car: ");
+let enginePower = prompt("Engine power (in hp): ");
+let licenseYear = prompt("What year did you get your drivers license: ");
+
+
+
+
+let array = [1, 2, 3, 6]
+
