901e49aa4f8077327bc79f7b4ce1bb0899450cc3
HursitTarcan <hursittarcan@gmail.com>
2022-07-31 22:42:52 +0200

    Challenge two - template done

--- challenge-two/template/app.js
+++ challenge-two/template/app.js
@@ -1,28 +1,16 @@
-//CHALLENGE ONE - SOLUTION
+//CHALLENGE ONE - TEMPLATE
 
 function checkScores(scores) {
-    let highestScore = scores[0];
-    let lowestScore = scores[0];
-    let totalAmountOfScores = 0;
-    let total = 0;
-
-    scores.forEach(score => {
-        total += score;
-        totalAmountOfScores++;
-
-        if (score >= highestScore) {
-            highestScore = score;
-        }
-        else if (score <= lowestScore) {
-            lowestScore = score;
-        }
-    });
+    //TODO: Add all necessary variables.
+    //TODO: Check for lowest and highest score in the array.
+    //TODO: Calculate the average score.
+    //TODO: Complete the console.log strings below, add the necessary variables to the end.
 
     console.log("TEST RESULTS");
     console.log("---------------------------------------------");
-    console.log("The highest score for this test is: " + highestScore);
-    console.log("The lowest score for this test is: " + lowestScore);
-    console.log("The average score for this test is: " + Math.round(total / totalAmountOfScores));
+    console.log("The highest score for this test is: ");
+    console.log("The lowest score for this test is: ");
+    console.log("The average score for this test is: ");
     console.log("---------------------------------------------");
 }
 
