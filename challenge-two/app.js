3755a64df6dbed1f89c5202e56853ea6315252af
HursitTarcan <hursittarcan@gmail.com>
2022-07-31 22:28:56 +0200

    challenge two updated

--- challenge-two/app.js
+++ challenge-two/app.js
@@ -4,14 +4,31 @@
 function checkScores(scores) {
     let highestScore = scores[0];
     let lowestScore = scores[0];
-    let totalToGet = scores.length * 10;
+    let averageScore = scores[0];
+    let totalAmountOfScores = 0;
     let total = 0;
 
     scores.forEach(score => {
-        console.log(score);
-    });
-}
+        total += score;
+        totalAmountOfScores++;
 
+        if (score >= highestScore) {
+            highestScore = score;
+        }
+        else if (score <= lowestScore) {
+            lowestScore = score;
+        }
+    });
+
+    averageScore = total / totalAmountOfScores;
+
+    console.log("TEST RESULTS");
+    console.log("---------------------------------------------");
+    console.log("The highest score for this test is: " + highestScore);
+    console.log("The lowest score for this test is: " + lowestScore);
+    console.log("The average score for this test is: " + Math.round(averageScore));
+    console.log("---------------------------------------------");
+}
 
 let scores = [8, 2, 7, 9, 6, 6, 4, 2, 3, 8, 10, 10, 5, 0, 4, 6]
 checkScores(scores)
