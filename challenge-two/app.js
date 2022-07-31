e8be4f06f31944db239ab6612ba398b9faab1742
HursitTarcan <hursittarcan@gmail.com>
2022-07-31 22:22:19 +0200

    challenge two changed -> first part done.

--- challenge-two/app.js
+++ challenge-two/app.js
@@ -1,8 +1,27 @@
-function checkIfNumbersAreEqual(numberOne, numberTwo) {
-    return numberOne === numberTwo;
+//teacher wants to fill in an array of scores.
+//Wants to get the highest / lowest / medium scores, wants to know how many people have above medium and how many have below medium.
+
+function checkScores(scores) {
+    let highestScore = scores[0];
+    let lowestScore = scores[0];
+    let totalToGet = scores.length * 10;
+    let total = 0;
+
+    scores.forEach(score => {
+        console.log(score);
+    });
 }
 
-let numberOne = 15;
-let numberTwo = 15;
 
-console.log(checkIfNumbersAreEqual(numberOne, numberTwo));
+let scores = [8, 2, 7, 9, 6, 6, 4, 2, 3, 8, 10, 10, 5, 0, 4, 6]
+checkScores(scores)
+
+//
+// function checkIfNumbersAreEqual(numberOne, numberTwo) {
+//     return numberOne === numberTwo;
+// }
+//
+// let numberOne = 15;
+// let numberTwo = 15;
+//
+// console.log(checkIfNumbersAreEqual(numberOne, numberTwo));
