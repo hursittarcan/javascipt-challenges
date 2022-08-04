44891faa6f73217a00b7a09e135f231820d9b25c
HursitTarcan <hursittarcan@gmail.com>
2022-08-04 20:52:19 +0200

    challenge 4 template done

--- 04/template/app.js
+++ 04/template/app.js
@@ -1,10 +1,12 @@
-//CHALLENGE FOUR - SOLUTION
+//CHALLENGE FOUR - TEMPLATE
 
 let fruits = ["Apple", "Kiwi", "Pineapple", "Pear", "Banana", "Orange", "Lemon", "Watermelon"];
 let fruitToSearch = "Kiwi";
 
-function findFruit(fruitsArray, fruitToSearch) {
-    return fruits.includes(fruitToSearch);
+function findFruit() {
+    //TODO: Add the necessary parameters to the function.
+    //TODO: Check if fruit type is available.
+    //TODO: Return a boolean value.
 }
 
 console.log(findFruit(fruits, fruitToSearch));
