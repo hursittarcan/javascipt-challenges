cb3adaffad6f28fc50a92d8e8afb7585f4256070
HursitTarcan <hursittarcan@gmail.com>
2022-08-04 17:33:28 +0200

    template + solution for challenge three done

---
+++ 03/template/app.js
@@ -0,0 +1,15 @@
+//CHALLENGE THREE - TEMPLATE
+
+let ownerName = "Hursit Tarcan";
+let ownerAge = 20;
+let carBrand = "BMW"
+let motorPower = 250;
+
+function calculateInsurance(age, power) {
+    //TODO: Calculate the price Hursit needs to pay.
+    //TODO: All this can be done in one single line. Using two ternary operators. (This is extra)
+    //TODO: Info about ternary operator: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+
+}
+
+console.log("User: " + ownerName + "\n" + "Car: " + carBrand + "\n" + "Total amount to pay: " + calculateInsurance(ownerAge, motorPower));
